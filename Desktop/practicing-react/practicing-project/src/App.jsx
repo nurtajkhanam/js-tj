89334420810ccab15a44c4e08ba7595639511ced
nurtaj <nurtajkhanam777@gmail.com>
2024-08-26 15:30:47 +0600

    statrting react

---
+++ Desktop/practicing-react/practicing-project/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1>learning React</h1>
+      <input type="text" placeholder="write your name:" />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default App;
